c5e857cba20b10daae890d0cd18af6638055e71a
wry@lapis <none@none>
2010-07-21 11:57:13 +0100

    correct lock format to match that of minicom

--- SerialLock.pm
+++ SerialLock.pm
@@ -45,7 +45,7 @@ sub slock($) {
 	my $lck = "/var/lock/LCK..".$dev;
 
 	return 0 if -f $lck;
-	# TODO determine whether lock is stale
+	# TODO determine whether lock is stale and try to override it
 
 	unless (open(LF, "+>$lck")) {
 		carp "cannot open $lck";
@@ -54,7 +54,10 @@ sub slock($) {
 	my $rv=0;
 
 	if (flock(LF, LOCK_EX)) {
-		print LF "$$ $0 $<\n";
+		my $me = $0;
+		$me =~ s,.*/,,;
+		my $u = (getpwuid($<))[0];
+		printf LF "%05u %s %s\n",($$,$me,$u);
 		$rv=1;
 	} else {
 		carp "cannot flock $lck";
